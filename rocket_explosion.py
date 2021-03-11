90d2c710d4d7f744d1831f2fedad9ce2f15c0928
Platypus2nd <platypus2nd@platypus.site>
2021-03-11 13:04:12 +0100

    ready, set, go sound fix

--- rocket_explosion.py
+++ rocket_explosion.py
@@ -10,7 +10,7 @@ class RocketExplosion:
         self.rocket_explosion_image = []
         self.rocket_destroyed = False
         self.explosion_sound = pygame.mixer.Sound('sounds/explosions/arcade_game_ship_explosion_simple.wav')
-        self.explosion_sound.set_volume(0.8)
+        self.explosion_sound.set_volume(1.0)
         for frame in range(1, 13):
             print('[i] Loading explosion: ')
             load_filename = 'images/explosion_animated/rocket_explosion_page_' \
@@ -21,16 +21,17 @@ class RocketExplosion:
 
     def redraw(self, rocket_explosion_x, rocket_explosion_y):
         if not self.rocket_destroyed:
-            if self.rocket_explosion_animation_frame_timer > 16:
-                self.rocket_explosion_animation_frame += 1
-                self.rocket_explosion_animation_frame_timer = 0
-            if self.rocket_explosion_animation_frame == 10:
-                self.rocket_explosion_animation_frame = 0
-                self.rocket_destroyed = True
-            if self.rocket_explosion_animation_frame == 0\
-                and self.rocket_explosion_animation_frame_timer == 0:
+            if self.rocket_explosion_animation_frame == 0 and self.rocket_explosion_animation_frame_timer == 0:
                 print("explosion sound", self.rocket_destroyed, self.rocket_explosion_animation_frame)
                 self.explosion_sound.play()
+            if self.rocket_explosion_animation_frame_timer > 16:
+                self.rocket_explosion_animation_frame_timer = 0
+                self.rocket_explosion_animation_frame += 1
             self.screen.blit(self.rocket_explosion_image[self.rocket_explosion_animation_frame],
                              (rocket_explosion_x - 16, rocket_explosion_y - 16))
             self.rocket_explosion_animation_frame_timer += 1
+            if self.rocket_explosion_animation_frame == 9:
+                self.rocket_explosion_animation_frame_timer = 0
+                self.rocket_explosion_animation_frame = 0
+                self.rocket_destroyed = True
+
